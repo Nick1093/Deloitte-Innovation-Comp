d0bb117e6b94d93f9e9b675733ba7b4ebb8499a8
Nicholas Lam <nicholasalexanderlam@gmail.com>
2023-01-10 11:01:04 -0500

    need css work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import "./App.css";
+import "./App.css";
 import PopUp from "./PopUp";
 
 function App() {
@@ -11,10 +11,69 @@ function App() {
 
   return (
     <>
-      <div className="chevron-left">x</div>
-      <div className="cart">Cart</div>
-      <button onClick={showPopUp}>Continue</button>
-      {popUp === true ? <PopUp></PopUp> : <div></div>}
+      <div className="v1_2">
+        <div className="v1_3">
+          <div className="v1_4"></div>
+          <div className="v1_5">
+            <div className="v1_6">
+              <div className="v1_7"></div>
+            </div>
+          </div>
+          <div className="v1_10">
+            <div className="v1_11"></div>
+            <div className="v1_12">
+              <img src="v1_12.png" alt="" />
+            </div>
+          </div>
+          <span className="v1_186">Brewed Coffee</span>
+        </div>
+        <div className="v1_23">
+          <div className="v1_24"></div>
+          <div className="v1_25">
+            <div className="v1_26">
+              <div className="v1_27"></div>
+            </div>
+          </div>
+          <div className="v1_30">
+            <div className="v1_31"></div>
+            <span className="v1_32">1</span>
+            <span className="v1_33">-</span>
+            <span className="v1_34">+</span>
+          </div>
+          <div className="v1_35">
+            <div className="v1_36"></div>
+            <span className="v1_37">1</span>
+            <span className="v1_38">-</span>
+            <span className="v1_39">+</span>
+          </div>
+          <div className="v1_45">
+            <div className="v1_46"></div>
+            <div className="v1_47">
+              <img src="v1_47.png" alt="" />
+            </div>
+          </div>
+          <div className="v1_188">
+            <div className="v1_189"></div>
+            <span className="v1_190">1</span>
+            <span className="v1_191">-</span>
+            <span className="v1_192">+</span>
+          </div>
+        </div>
+        <span className="v1_49">Cart</span>
+        <div className="v1_50"></div>
+        <div className="v1_155"></div>
+        <span className="v1_158">+ Add items</span>
+        <button className="v1_195" onClick={showPopUp}>
+          Continue
+        </button>
+        {popUp === true ? <PopUp></PopUp> : <div></div>}
+        <div className="v1_53">
+          <div className="v1_54"></div>
+        </div>
+        <span className="v1_187">$1.83 * 4 Cals</span>
+        <span className="v1_193">20 Assorted Timbits</span>
+        <span className="v1_194">$4.99 * 2,000 Cals</span>
+      </div>
     </>
   );
 }
--- src/PopUp.js
+++ src/PopUp.js
@@ -3,7 +3,12 @@ import React from "react";
 function PopUp() {
   return (
     <>
-      <div>PopUp</div>
+      <div>Invest in Personal Goals</div>
+      <input type="radio" value="Round to the next dollar $" />
+      Round to the next dollar
+      <input type="radio" value="Round to the next 2 dollars $" />
+      Round to the next 2 dollars $<button>Cancel</button>
+      <button>Proceed</button>
     </>
   );
 }
