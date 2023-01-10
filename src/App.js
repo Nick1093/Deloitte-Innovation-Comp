0a830b6c781a8cd65a9dde1e0736590773d81242
Nicholas Lam <nicholasalexanderlam@gmail.com>
2023-01-10 12:40:17 -0500

    new pushes, need to work on button pop up

--- src/App.js
+++ src/App.js
@@ -11,68 +11,66 @@ function App() {
 
   return (
     <>
-      <div className="v1_2">
-        <div className="v1_3">
-          <div className="v1_4"></div>
-          <div className="v1_5">
-            <div className="v1_6">
-              <div className="v1_7"></div>
+      <div className="v1_356">
+        <div className="v1_357">
+          <div className="v1_358"></div>
+          <div className="v1_359">
+            <div className="v1_360">
+              <div className="v1_361"></div>
             </div>
           </div>
-          <div className="v1_10">
-            <div className="v1_11"></div>
-            <div className="v1_12">
-              <img src="v1_12.png" alt="" />
-            </div>
+          <div className="v1_362">
+            <div className="v1_363"></div>
+            <div className="v1_364"></div>
           </div>
-          <span className="v1_186">Brewed Coffee</span>
+          <span className="v1_365">Brewed Coffee</span>
         </div>
-        <div className="v1_23">
-          <div className="v1_24"></div>
-          <div className="v1_25">
-            <div className="v1_26">
-              <div className="v1_27"></div>
+        <div className="v1_366">
+          <div className="v1_367"></div>
+          <div className="v1_368">
+            <div className="v1_369">
+              <div className="v1_370"></div>
             </div>
           </div>
-          <div className="v1_30">
-            <div className="v1_31"></div>
-            <span className="v1_32">1</span>
-            <span className="v1_33">-</span>
-            <span className="v1_34">+</span>
+          <div className="v1_371">
+            <div className="v1_372"></div>
+            <span className="v1_373">1</span>
+            <span className="v1_374">-</span>
+            <span className="v1_375">+</span>
           </div>
-          <div className="v1_35">
-            <div className="v1_36"></div>
-            <span className="v1_37">1</span>
-            <span className="v1_38">-</span>
-            <span className="v1_39">+</span>
+          <div className="v1_376">
+            <div className="v1_377"></div>
+            <span className="v1_378">1</span>
+            <span className="v1_379">-</span>
+            <span className="v1_380">+</span>
           </div>
-          <div className="v1_45">
-            <div className="v1_46"></div>
-            <div className="v1_47">
-              <img src="v1_47.png" alt="" />
-            </div>
+          <div className="v1_381">
+            <div className="v1_382"></div>
           </div>
-          <div className="v1_188">
-            <div className="v1_189"></div>
-            <span className="v1_190">1</span>
-            <span className="v1_191">-</span>
-            <span className="v1_192">+</span>
+          <div className="v1_385">
+            <div className="v1_386"></div>
+            <span className="v1_387">1</span>
+            <span className="v1_388">-</span>
+            <span className="v1_389">+</span>
           </div>
         </div>
-        <span className="v1_49">Cart</span>
-        <div className="v1_50"></div>
-        <div className="v1_155"></div>
-        <span className="v1_158">+ Add items</span>
-        <button className="v1_195" onClick={showPopUp}>
+        <div className="v1_383"></div>
+        <span className="v1_390">Cart</span>
+        <div className="v1_391"></div>
+        <div className="v1_392"></div>
+        <span className="v1_393">+ Add items</span>
+        {/* <span className="v1_394">Continue</span> */}
+        <span className="v1_394" onClick={showPopUp}>
           Continue
-        </button>
+        </span>
         {popUp === true ? <PopUp></PopUp> : <div></div>}
-        <div className="v1_53">
-          <div className="v1_54"></div>
+        <div className="v1_395">
+          <div className="v1_396"></div>
         </div>
-        <span className="v1_187">$1.83 * 4 Cals</span>
-        <span className="v1_193">20 Assorted Timbits</span>
-        <span className="v1_194">$4.99 * 2,000 Cals</span>
+        <span className="v1_397">$1.83 * 4 Cals</span>
+        <span className="v1_398">20 Assorted Timbits</span>
+        <span className="v1_399">$4.99 * 2,000 Cals</span>
+        <div className="v1_342"></div>
       </div>
     </>
   );
--- src/PopUp.js
+++ src/PopUp.js
@@ -1,14 +1,32 @@
 import React from "react";
+import "./PopUp.css";
 
 function PopUp() {
   return (
     <>
-      <div>Invest in Personal Goals</div>
-      <input type="radio" value="Round to the next dollar $" />
-      Round to the next dollar
-      <input type="radio" value="Round to the next 2 dollars $" />
-      Round to the next 2 dollars $<button>Cancel</button>
-      <button>Proceed</button>
+      <div className="container">
+        <div className="v1_343"></div>
+        <span className="v1_345">Invest in RBC mutual fund</span>
+        <div className="v1_344"></div>
+        <span className="v3_403">Invest in Personal Goal</span>
+        {/* <div className="v1_349">
+          <div className="name"></div>
+          <div className="v1_351"></div>
+          <div className="v1_352"></div>
+        </div> */}
+        <input className="v1_353" type="radio" value="Round to the nearest $" />
+        Round to the nearest $
+        <input
+          className="v1_354"
+          type="radio"
+          value="Invest 10% of your order"
+        />
+        Invest 10% of your order
+        <div className="v1_355"></div>
+        <div className="v1_346"></div>
+        <span className="v1_348">Cancel</span>
+        <span className="v1_347">Proceed</span>
+      </div>
     </>
   );
 }
